e977f1bc77b0696d7569523dbc9f8416814e3ee1
Daichi Kato <daichi5967@docomo.ne.jp>
2022-02-16 17:13:44 +0900

    feat: input helper

---
+++ constants/move.py
@@ -0,0 +1,10 @@
+from enum import Enum, auto
+
+class MOVE(Enum):
+    LEFT = auto()
+    RIGHT = auto()
+    DOWN = auto()
+    DROP = auto()
+    HOLD = auto()
+    R_ROT = auto()
+    L_ROT = auto()
---
+++ helpers/input.py
@@ -0,0 +1,40 @@
+import pyautogui
+import time
+from constants.move import MOVE
+
+KEY_HOLD_TIME = 0.05
+
+def Move (move:MOVE):
+    if move is MOVE.LEFT:
+        pyautogui.keyDown("left")
+        time.sleep(KEY_HOLD_TIME)
+        pyautogui.keyUp("left")
+    elif move is MOVE.RIGHT:
+        pyautogui.keyDown("right")
+        time.sleep(KEY_HOLD_TIME)
+        pyautogui.keyUp("right")
+    elif move is MOVE.DOWN:
+        pyautogui.keyDown("down")
+        time.sleep(KEY_HOLD_TIME)
+        pyautogui.keyUp("down")
+    elif move is MOVE.DROP:
+        pyautogui.keyDown("space")
+        time.sleep(KEY_HOLD_TIME)
+        pyautogui.keyUp("space")
+    elif move is MOVE.HOLD:
+        pyautogui.keyDown("c")
+        time.sleep(KEY_HOLD_TIME)
+        pyautogui.keyUp("c")
+    elif move is MOVE.R_ROT:
+        pyautogui.keyDown("x")
+        time.sleep(KEY_HOLD_TIME)
+        pyautogui.keyUp("x")
+    elif move is MOVE.L_ROT:
+        pyautogui.keyDown("z")
+        time.sleep(KEY_HOLD_TIME)
+        pyautogui.keyUp("z")
+
+def PressEnter ():
+    pyautogui.keyDown("Enter")
+    time.sleep(KEY_HOLD_TIME)
+    pyautogui.keyUp("Enter")
