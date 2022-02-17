61b85ff2de020fbd7a62e136a9b5c16364530720
Daichi Kato <daichi5967@docomo.ne.jp>
2022-02-18 02:25:54 +0900

    feat: Correspond to any window size

--- init.py
+++ init.py
@@ -1,17 +1,11 @@
-from constants.position import WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT
+from constants.position import WINDOW_X, WINDOW_Y
 import pyautogui
-import os
 import time
 
 def Init():
+    # pyautoguiの遅延を0にする
     pyautogui.PAUSE = 0
 
-    # windowサイズを固定する
-    window = "{{{},{},{},{}}}".format(WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT)
-    os.system(
-        f"osascript -e 'tell application \"Parallels Desktop\"' -e 'set bounds of front window to {window}' -e 'end tell'"
-    )
-
     # windowをアクティブにする
-    pyautogui.click(100,100)
+    pyautogui.click(WINDOW_X + 1, WINDOW_Y + 1)
     time.sleep(0.5)
