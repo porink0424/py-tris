52e9528a9c4ee4d5269ac74322ee7ab43537b7a8
estodi <sunt@g.ecc.u-tokyo.ac.jp>
2022-03-03 15:34:49 +0900

    feat: added new vars to Board

--- lib/classes/board.py
+++ lib/classes/board.py
@@ -8,14 +8,18 @@ class Board():
         currentMino : Union[DirectedMino, None] = None,
         followingMinos : List[MINO] = None,
         holdMino : MINO = None,
-        canHold:bool = True
+        canHold:bool = True,
+        minoCounter : Union[int, None] = None,
+        minoBagContents : Union[List[MINO], None] = None
     ) -> None:
         self.mainBoard = mainBoard if mainBoard is not None else [[MINO.NONE for _ in range(BOARD_WIDTH)] for _ in range(BOARD_HEIGHT)]
         self.followingMinos = followingMinos if followingMinos is not None else [MINO.NONE for _ in range(FOLLOWING_MINOS_COUNT)]
         self.currentMino = currentMino if currentMino is not None else None
         self.holdMino = holdMino if holdMino is not None else MINO.NONE
         self.canHold = canHold
-    
+        self.minoCounter = minoCounter # 0
+        self.minoBagContents = minoBagContents # ReturnFullBag()
+
     # mainBoardの任意の場所にブロックを足す
     def AddMinoToMainBoard (self, pos:Tuple[int], mino:MINO):
         self.mainBoard[pos[1]][pos[0]] = mino
