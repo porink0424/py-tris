b4433f33ca1c647edefb6cd9bf9a0bf8e86c2a7b
Daichi Kato <daichi5967@docomo.ne.jp>
2022-03-20 17:20:04 +0900

    feat: Build server as a child process

--- gui/index.js
+++ gui/index.js
@@ -2,6 +2,20 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
+// serverを立てる
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+const node = spawn(
+    'node',
+    ['server.js']
+);
+node.stdout.on('data', (data) => {
+    process.stdout.write(data);
+    fs.writeFile("output.txt", data, {flag: 'a'}, (err) => {
+        if (err) throw err;
+    });
+})
+
 // windowを作成
 app.on('ready', () => {
     // mainWindowを作成
@@ -11,5 +25,6 @@ app.on('ready', () => {
 
     mainWindow.on('closed', function() {
         mainWindow = null;
+        node.kill();
     });
 });
